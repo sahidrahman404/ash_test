c0137df79f7c82ce3d4f2fe4327231e2b008c863
Sahid Rahman <sahidrahman404@gmail.com>
2023-02-01 15:20:42 +0700

    add graphql to the router

--- lib/ash_test_web/router.ex
+++ lib/ash_test_web/router.ex
@@ -2,25 +2,36 @@ defmodule AshTestWeb.Router do
   use AshTestWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", AshTestWeb do
-    pipe_through :api
+    pipe_through(:api)
   end
 
   scope "/api/json" do
     pipe_through(:api)
 
-    forward "/ashtest", AshTestWeb.Support.Router
+    forward("/ashtest", AshTestWeb.Support.Router)
+  end
+
+  scope "/" do
+    forward("/gql", Absinthe.Plug, schema: AshTestWeb.Support.Schema)
+
+    forward(
+      "/playground",
+      Absinthe.Plug.GraphiQL,
+      schema:  AshTestWeb.Support.Schema,
+      interface: :playground
+    )
   end
 
   # Enable Swoosh mailbox preview in development
   if Application.compile_env(:ash_test, :dev_routes) do
     scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
+      pipe_through([:fetch_session, :protect_from_forgery])
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end
