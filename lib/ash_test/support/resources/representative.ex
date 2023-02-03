de82e0b922a60181024cb3461ab963bf5884e79f
Sahid Rahman <sahidrahman404@gmail.com>
2023-02-03 09:38:25 +0700

    update representative graphql

--- lib/ash_test/support/resources/representative.ex
+++ lib/ash_test/support/resources/representative.ex
@@ -1,9 +1,28 @@
 defmodule AshTest.Support.Representative do
-  use Ash.Resource, data_layer: AshPostgres.DataLayer
+  use Ash.Resource,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshGraphql.Resource]
+
+  graphql do
+    type(:representative)
+
+    queries do
+      # <- create a field called `get_ticket` that uses the `read` read action to fetch a single post
+      get(:get_rep, :read)
+
+      # <- create a field called `list_tickets` that uses the `read` read action to fetch a list of posts
+      list(:list_rep, :read)
+    end
+
+    mutations do
+      # And so on
+      create(:create_rep, :create)
+    end
+  end
 
   postgres do
-    table "representatives"
-    repo AshTest.Repo
+    table("representatives")
+    repo(AshTest.Repo)
   end
 
   actions do
@@ -18,7 +37,7 @@ defmodule AshTest.Support.Representative do
   end
 
   relationships do
-    has_many :tickets, AshTest.Support.Ticket
+    has_many(:tickets, AshTest.Support.Ticket)
   end
 
   code_interface do
